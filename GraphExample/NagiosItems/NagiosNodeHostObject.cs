8e9909262f417ab531189bfa663475ee3a486554
Fox-Alpha <fox-alpha@tarnhoerner.de>
2016-11-15 22:03:20 +0100

    Add: Weitere Items ins Object aufgenommen

--- GraphExample/NagiosItems/NagiosNodeHostObject.cs
+++ GraphExample/NagiosItems/NagiosNodeHostObject.cs
@@ -11,19 +11,25 @@ namespace GraphNodes.NagiosItems
 {
 	public sealed class NagiosNodeHostObject : Node
 	{
-		public NagiosNodeHostObject(string title) :
+		public NagiosNodeHostObject (string title) :
 			base (title)
 		{
-			this.AddItem (new NodeTextBoxItem ("Hostname", false, false));
-			this.AddItem (new NodeLabelItem ("Hostgroups", false, false));
-			this.AddItem (new NodeLabelItem ("HostCheckCommand", false, true));
+			this.AddItem (new NodeLabelItem ("use", false, true) { Tag = "NagiosHostTemplate"});
+			this.AddItem (new NodeTextBoxItem ("host_name", false, false));
+			this.AddItem (new NodeTextBoxItem ("alias", false, false));
+			this.AddItem (new NodeTextBoxItem ("adress", false, false));
+			this.AddItem (new NodeLabelItem ("contacts", false, true) { Tag = "NagiosContact"});
+			this.AddItem (new NodeLabelItem ("check_command", false, true) { Tag = "NagiosCheckCommand"});
+			this.AddItem (new NodeLabelItem ("contact_groups", false, true) { Tag = "NagiosContactGroup" });
+			this.AddItem (new NodeLabelItem ("notification_period", false, true) { Tag = "NagiosTimePeriod" });
+			this.AddItem (new NodeLabelItem ("check_period", false, true) { Tag = "NagiosTimePeriod" });
+			this.AddItem (new NodeCheckboxItem ("active_checks_enables", false, false));
+			this.AddItem (new NodeCheckboxItem ("passive_checks_enables", false, false));
+
+			this.AddItem (new NodeNumericSliderItem ("max_check_attemps", 64.0f, 16.0f, 1, 10.0f, 1.0f, false, false));
+
 			this.AddItem (new NodeTextBoxItem ("Host Options", false, true));
 			this.AddItem (new NodeDropDownItem (new string [] { "OK", "WARNING", "CRITICAL", "UNKNOWN" }, 0, false, false));
-		}
-
-		public Node GetHostObjectNode ()
-		{
-			return this;
 		}
 	}
 }
