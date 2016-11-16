9cb97c9fccafa51dd4360aaa911b9cb0707b1e97
Fox-Alpha <Fox-Alpha@Tarnhoerner.de>
2016-11-16 09:15:05 +0100

    Add: Object für Kontakt erstellt

--- GraphExample/NagiosItems/NagiosNodeHostObject.cs
+++ GraphExample/NagiosItems/NagiosNodeHostObject.cs
@@ -15,11 +15,11 @@ namespace GraphNodes.NagiosItems
 			base (title)
 		{
 			this.AddItem (new NodeLabelItem ("use", true, false) { Tag = "NagiosHostTemplate"});
-			this.AddItem (new NodeTextBoxItem ("host_name", false, false));
+			this.AddItem (new NodeTextBoxItem ("host_name", false, false) { IsImportantNodeItem = true});
 			this.AddItem (new NodeTextBoxItem ("alias", false, false));
 			this.AddItem (new NodeTextBoxItem ("adress", false, false));
 			this.AddItem (new NodeLabelItem ("contacts", true, false) { Tag = "NagiosContact"});
-			this.AddItem (new NodeLabelItem ("check_command", true, false) { Tag = "NagiosCheckCommand"});
+			this.AddItem (new NodeLabelItem ("check_command", true, false) { Tag = "NagiosCheckCommand", IsImportantNodeItem = true});
 			this.AddItem (new NodeLabelItem ("contact_groups", true, false) { Tag = "NagiosContactGroup" });
 			this.AddItem (new NodeLabelItem ("notification_period", true, false) { Tag = "NagiosTimePeriod" });
 			this.AddItem (new NodeLabelItem ("check_period", true, false) { Tag = "NagiosTimePeriod" });
@@ -32,4 +32,31 @@ namespace GraphNodes.NagiosItems
 			this.AddItem (new NodeDropDownItem (new string [] { "OK", "WARNING", "CRITICAL", "UNKNOWN" }, 0, false, false));
 		}
 	}
+
+    //  Hosttemplaze
+    //  Hostgroup
+
+    public sealed class NagiosNodeContactObject : Node
+    {
+        public NagiosNodeContactObject (string title) :
+            base (title)
+        {
+            this.AddItem (new NodeLabelItem ("use", true, false) { Tag = "NagiosContactTemplate" });
+            this.AddItem (new NodeTextBoxItem ("contact_name", false, true) { Tag = "NagiosContact"});
+            this.AddItem (new NodeTextBoxItem ("alias", false, false));
+            this.AddItem (new NodeLabelItem ("service_notification_period", true, false) { Tag = "NagiosTimePeriod" });
+            this.AddItem (new NodeLabelItem ("host_notification_period", true, false) { Tag = "NagiosTimePeriod" });
+            this.AddItem (new NodeTextBoxItem ("email", false, false));
+
+            this.AddItem (new NodeLabelItem ("service_notification_commands", true, false) { Tag = "NagiosCheckCommand" });
+            this.AddItem (new NodeLabelItem ("host_notification_commands", true, false) { Tag = "NagiosCheckCommand" });
+
+            this.AddItem (new NodeCheckboxItem ("host_notifications_enabled", false, false));
+            this.AddItem (new NodeCheckboxItem ("service_notifications_enabled", false, false));
+
+            this.AddItem (new NodeLabelItem ("service_notification_options", true, false) { Tag = "NagiosNotificationOption" });
+            this.AddItem (new NodeLabelItem ("host_notification_options", true, false) { Tag = "NagiosNotificationOption" });
+        }
+    }
+
 }
