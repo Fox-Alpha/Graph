f9697be0c5b6c0cedef9f81acd8cc60b7a7efb66
Fox-Alpha <Fox-Alpha@Tarnhoerner.de>
2016-11-16 08:08:38 +0100

    Chg: Input- statt Output Verbindungen verwenden

--- GraphExample/NagiosItems/NagiosNodeHostObject.cs
+++ GraphExample/NagiosItems/NagiosNodeHostObject.cs
@@ -14,21 +14,21 @@ namespace GraphNodes.NagiosItems
 		public NagiosNodeHostObject (string title) :
 			base (title)
 		{
-			this.AddItem (new NodeLabelItem ("use", false, true) { Tag = "NagiosHostTemplate"});
+			this.AddItem (new NodeLabelItem ("use", true, false) { Tag = "NagiosHostTemplate"});
 			this.AddItem (new NodeTextBoxItem ("host_name", false, false));
 			this.AddItem (new NodeTextBoxItem ("alias", false, false));
 			this.AddItem (new NodeTextBoxItem ("adress", false, false));
-			this.AddItem (new NodeLabelItem ("contacts", false, true) { Tag = "NagiosContact"});
-			this.AddItem (new NodeLabelItem ("check_command", false, true) { Tag = "NagiosCheckCommand"});
-			this.AddItem (new NodeLabelItem ("contact_groups", false, true) { Tag = "NagiosContactGroup" });
-			this.AddItem (new NodeLabelItem ("notification_period", false, true) { Tag = "NagiosTimePeriod" });
-			this.AddItem (new NodeLabelItem ("check_period", false, true) { Tag = "NagiosTimePeriod" });
+			this.AddItem (new NodeLabelItem ("contacts", true, false) { Tag = "NagiosContact"});
+			this.AddItem (new NodeLabelItem ("check_command", true, false) { Tag = "NagiosCheckCommand"});
+			this.AddItem (new NodeLabelItem ("contact_groups", true, false) { Tag = "NagiosContactGroup" });
+			this.AddItem (new NodeLabelItem ("notification_period", true, false) { Tag = "NagiosTimePeriod" });
+			this.AddItem (new NodeLabelItem ("check_period", true, false) { Tag = "NagiosTimePeriod" });
 			this.AddItem (new NodeCheckboxItem ("active_checks_enables", false, false));
 			this.AddItem (new NodeCheckboxItem ("passive_checks_enables", false, false));
 
-			this.AddItem (new NodeNumericSliderItem ("max_check_attemps", 64.0f, 16.0f, 1, 10.0f, 1.0f, false, false));
+			this.AddItem (new NodeNumericSliderItem ("max_check_attemps", 55.0f, 16.0f, 1, 10.0f, 1.0f, false, false) { ShowNumeric = true});
 
-			this.AddItem (new NodeTextBoxItem ("Host Options", false, true));
+			this.AddItem (new NodeTextBoxItem ("Host Options", true, false));
 			this.AddItem (new NodeDropDownItem (new string [] { "OK", "WARNING", "CRITICAL", "UNKNOWN" }, 0, false, false));
 		}
 	}
