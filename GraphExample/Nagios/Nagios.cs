7a2660e5d45c36813889d3d00c0795a4dcf911ad
Fox-Alpha <fox-alpha@tarnhoerner.de>
2016-11-20 01:54:50 +0100

    Add: Erweitern der Nagiosklasse mit Funktionen zum Laden einer JSON Datei. Hinzufügen der JSON Properties

--- GraphExample/Nagios/Nagios.cs
+++ GraphExample/Nagios/Nagios.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,17 +15,45 @@ using Newtonsoft.Json;
 namespace GraphNodes.Nagios
 {
 	[JsonObject (MemberSerialization.OptIn, Title = "nagios")]
-	public class Nagios
+	public class NagiosJSON
 	{
-		[JsonProperty ("")]
+		[JsonProperty ("nagiosobjects")]
 		List<string> listNagiosObjects;
-		[JsonProperty ("")]
-		List<NagiosNode> listNagiosNodes;
-		[JsonProperty ("")]
+		[JsonProperty ("nagiosnodes")]
+		Dictionary<string, NagiosNode> listNagiosNodes;
+		[JsonProperty ("nagiosnodeitems")]
 		List<NagiosNodeItem> listNagiosNodeItems;
 
-		public Nagios ()
+		public string ObjectDirectory
 		{
+			get
+			{
+				//return Path.Combine (System.Reflection.Assembly.GetExecutingAssembly ().GetName ().Name);
+				//return Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly ().GetName ().FullName);
+				return AppDomain.CurrentDomain.BaseDirectory;
+			}
+		}
+		public string ObjectPath { get { return Path.Combine (new string [] { ObjectDirectory, "Nagios", "Nagios_Nodes_ItemNodes.json" } );  } }
+
+		public NagiosJSON ()
+		{
+			if (LoadSettings ())
+			{
+				Debug.WriteLine ("JSON wurde geladen", "NAGIOS");
+			}
+			else
+				Debug.WriteLine ("Laden dere JSON Settings fehlgeschlagen", "NAGIOS");
+		}
+		public bool LoadSettings ()
+		{
+			if (File.Exists (ObjectPath))
+			{
+				JsonConvert.PopulateObject (File.ReadAllText (ObjectPath, Encoding.UTF8), this);
+				return true;
+			}
+			else
+				return false;
+
 		}
 	}
 }
@@ -33,7 +63,7 @@ namespace Graphnodes.Nagios.Node
 	[JsonObject (MemberSerialization.OptIn, Title = "nagiosnodes")]
 	public class NagiosNode
 	{
-		[JsonProperty ("")]
+		[JsonProperty ("nodeitems")]
 		List<string> listNagiosNodeItems;
 
 		public NagiosNode()
@@ -48,9 +78,9 @@ namespace Graphnodes.Nagios.NodeItem
 	[JsonObject (MemberSerialization.OptIn, Title = "nagiosnodeitems")]
 	public class NagiosNodeItem
 	{
-		[JsonProperty ("")]
+		[JsonProperty ("important")]
 		List<NagiosNodeItemObject> nagiosImportantNodeItemObjects;
-		[JsonProperty ("")]
+		[JsonProperty ("additional")]
 		List<NagiosNodeItemObject> nagiosAdditionalNodeItemObjects;
 
 		public NagiosNodeItem ()
@@ -62,23 +92,23 @@ namespace Graphnodes.Nagios.NodeItem
 	[JsonObject (MemberSerialization.OptIn, Title = "nagiosnodeitemobjects")]
 	public class NagiosNodeItemObject
 	{
-		[JsonProperty ("")]
+		[JsonProperty ("itemname")]
 		public string itemName { get; set; } = "";
-		[JsonProperty ("")]
+		[JsonProperty ("itemtype")]
 		public string itemtype { get; set; } = "";  //	TODO: String2Emum
-		[JsonProperty ("")]
+		[JsonProperty ("input")]
 		public bool hasInputConnector { get; set; } = false;
-		[JsonProperty ("")]
+		[JsonProperty ("output")]
 		public bool hasOutputConnector { get; set; } = false;
-		[JsonProperty ("")]
+		[JsonProperty ("tag")]
 		public string itemTag { get; set; } = "";
-		[JsonProperty ("")]
+		[JsonProperty ("isTitel")]
 		public bool isTitel { get; set; } = false;
-		[JsonProperty ("")]
+		[JsonProperty ("important")]
 		public bool isMandatory { get; set; } = false;
-		[JsonProperty ("")]
+		[JsonProperty ("names")]
 		public List<string> itemsNames;
-		[JsonProperty ("")]
+		[JsonProperty ("values")]
 		public List<string> itemsValues;
 
 		public NagiosNodeItemObject ()
